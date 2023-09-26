7d4ef5656213f315ecedba567fe0df08af3dd0ea
terrencejihoonjung <terrencejung928@gmail.com>
2023-09-26 11:36:39 -0700

    setting up client side

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import InputTodo from "./components/InputTodo";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <InputTodo />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/InputTodo.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function InputTodo() {
+  const [description, setDescription] = useState("");
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    setDescription(e.target.value);
+  }
+
+  async function handleSubmit(e: React.ChangeEvent<HTMLInputElement>): void {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:3000/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          body: JSON.stringify(body),
+        },
+      });
+
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  return (
+    <>
+      <h1 className="text-4xl mt-5">Pern Todo List</h1>
+      <form className="w-1/2 mt-5" onSubmit={handleSubmit}>
+        <label
+          htmlFor="price"
+          className="text-sm font-medium leading-6 text-gray-900"
+        >
+          Create a Todo
+        </label>
+        <div className="flex mt-2 rounded-md shadow-sm">
+          <input
+            type="text"
+            value={description}
+            onChange={handleInputChange}
+            name="todo"
+            id="todo-input"
+            className="w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Write a Todo"
+          />
+          <button className="px-4 mx-1 rounded-md bg-indigo-500 text-slate-50">
+            Add
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default InputTodo;
